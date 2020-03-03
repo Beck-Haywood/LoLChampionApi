0ba53de995423fa793dd75790dc2756901d1c1c4
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-03-03 12:04:02 -0800

    AUTH tests, working on champ tests

--- src/test/champ.js
+++ src/test/champ.js
@@ -2,7 +2,13 @@ const app = require("../index.js");
 const mongoose = require('mongoose');
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
 const chaiHttp = require("chai-http");
+const expect = chai.expect;
+var request = require('request');
 
+
+const Champ = require('../models/champ');
+const server = require('../index');
+const User = require('../models/user')
 chai.config.includeStack = true;
 
 chai.use(chaiHttp);
@@ -18,12 +24,15 @@ after((done) => {
   done();
 });
 
-describe('## Thing APIs', () => {
+describe('## Champ APIs', () => {
   // TODO: Implement more tests.
+  it("checks equality", function() {
+    expect(true).to.be.true;
+  });
 
   it('should load homepage', () => {
     chai.request(app)
-      .get('/api/thing')
+      .get('/api/champ/')
       .end((err, res) => {
         if (err) {
           return done(err);
@@ -32,4 +41,15 @@ describe('## Thing APIs', () => {
         return done();
       })
   })
+});
+// In this test it's expected a task list of two tasks
+describe('GET /api/champ', function() {
+  it('returns a list of champs', function(done) {
+      request.get('/api/champ')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.have.lengthOf(3);
+              done(err);
+          });
+  });
 });
